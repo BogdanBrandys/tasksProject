aea73e1008737eb98e940856849946fa463ed05a
Bogdan Brandys <brandpol@poczta.onet.pl>
2024-08-14 21:45:09 +0200

    18.3 Additional task - prime numbers

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,5 +1,6 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.PrimeNumbers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,9 @@ import java.util.List;
 @SpringBootApplication
 public class TasksApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
+		PrimeNumbers primeNumbers = new PrimeNumbers();
+		int number = 17;
+		System.out.println("Number " + number + " is Prime: " + primeNumbers.isPrime(number));
+		//SpringApplication.run(TasksApplication.class, args);
 	}
 }
---
+++ src/main/java/com/crud/tasks/domain/PrimeNumbers.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.domain;
+
+public class PrimeNumbers {
+    public boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
